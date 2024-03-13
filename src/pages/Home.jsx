eda10ddccc639db2280470c56287c8fdf9bdeb0c
jujusoi <jalxmcdonald@hotmail.com>
2024-03-13 19:20:05 +1030

    made some drastic changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,45 @@
 export default function Home() {
   return (
     <>
-    <section style={{height: 700, width: '100%', backgroundColor: '#FDF8F5' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', color: 'black' }}>  
-        <div className='main-div' style={{ border: '1px solid black' }}>hello there</div>
-        <div className='main-div' style={{ border: '1px solid black' }}>hello there</div>
-      </div>
+    <section style={{ width: '100%', backgroundColor: '#FDF8F5' }}>
+      <section style={{  height: 410, backgroundColor: '#c43c3c' }} id="introbg-h">
+        <section style={{ height: '100%', width: '53%', margin: 'auto', display: 'flex', justifyContent: 'flex-end'}}>
+          <div style={{ backgroundColor: '#ea332f',  width: '50%', height: '65%',  border: '1px solid black', marginTop: 115}}>
+            <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+            <h2>Hello hey hey hey hehy ehy heyhe hyehy heyh ehyehyheh</h2>
+            <p style={{ overflowWrap: 'anywhere'}}>yoyoyoyoyoyoyoyoyoyoyoyoy oyoyoyoyoyoyoyoyoyo yoyoyoyoyoyoyoyoyoyoyo</p>
+            </div>
+          </div>
+        </section>
+      </section>
+      <section style={{  height: 225, backgroundColor: '#ea332f' }} id="introblnk-h">
+        <section style={{ height: '100%', width: '53%', margin: 'auto', display: 'flex'}}>
+          <div style={{ margin: 'auto', width: '65%', height: '70%',  border: '1px solid black'}}></div>
+          <div style={{ margin: 'auto', width: '30%', height: '70%',  border: '1px solid black'}}></div>
+        </section>
+      </section>
+      <section style={{  backgroundColor: '#c43c3c' }} id="menulocgal-h">
+        <section style={{ height: '250px', width: '53%', margin: 'auto', display: 'flex'}}>
+            <div style={{ margin: 'auto auto 50px auto', width: '100%', height: '50%',  border: '1px solid black'}}></div>
+        </section>
+        <section style={{ height: '300px', width: '45%', margin: 'auto', display: 'flex'}}>
+          <div style={{ margin: 'auto', width: '100%', height: '60%',  border: '1px solid black', backgroundColor: '#4d40ff'}}></div>
+        </section>
+      </section>
+      <section style={{backgroundColor: '#ea332f'}} id="gall-h">
+        <section style={{ height: '400px', width: '45%', margin: 'auto', display: 'flex'}}>
+        </section>
+      </section>
+      <section style={{backgroundColor: '#4d40ff'}} id="abus-h">
+        <section style={{ height: '275px', width: '53%', margin: 'auto', display: 'flex'}}>
+          <div style={{ margin: 'auto', width: '100%', height: '60%',  border: '1px solid black', backgroundColor: '#4d40ff'}}></div>
+        </section>
+      </section>
+      <section style={{backgroundColor: '#c43c3c'}} id="abus-h">
+        <section style={{ height: '125px', width: '53%', margin: 'auto', display: 'flex'}}>
+        </section>
+      </section>
     </section>
     </>
   )
-}
+};
